63db71d396bb590bb6c238714f7665f03398ac79
Enl1sh <46706168+Enl1sh@users.noreply.github.com>
2019-03-05 17:53:05 +0100

    Portfolio

---
+++ 07.portfolio/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+$('.owl-carousel').owlCarousel({
+	items:6,
+	loop:true,
+	autoplayTimeout:7000,
+	autoplay: true,
+	autoplaySpeed: 500,
+})
+
+/*$('.owl-carousel').owlCarousel({
+    animateOut: 'slideOutDown',
+    animateIn: 'flipInX',
+    items:1,
+    margin:30,
+    stagePadding:30,
+    smartSpeed:450
+});*/
